5de981053bf10405f6e2710f9ef1a23344f301c8
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-07-17 15:22:50 +0300

    Yazı listesi ve yazı detayı oluşturuldu. react-router-dom ve axios ile API'den veri dinamik olarak çekildi.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
+import React from "react";
+import YaziListesi from "./components/YaziListesi";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import YaziDetayi from "./components/YaziDetayi";
+
 function App() {
   return (
-    <div className="main-wrapper">
-      <header></header>
-      <div class="ui raised very padded text container segment">
-        <div class="ui relaxed divided list">
-          <div class="item">
-            <i class="large github middle aligned icon"></i>
-            <div class="content">
-              <a class="header">Semantic-Org/Semantic-UI</a>
-              <div class="description">Updated 10 mins ago</div>
-            </div>
-          </div>
-          <div class="item">
-            <i class="large github middle aligned icon"></i>
-            <div class="content">
-              <a class="header">Semantic-Org/Semantic-UI-Docs</a>
-              <div class="description">Updated 22 mins ago</div>
-            </div>
-          </div>
-          <div class="item">
-            <i class="large github middle aligned icon"></i>
-            <div class="content">
-              <a class="header">Semantic-Org/Semantic-UI-Meteor</a>
-              <div class="description">Updated 34 mins ago</div>
-            </div>
-          </div>
+    <Router>
+      <div className="main-wrapper">
+        <header></header>
+        <div className="ui raised very padded text container segment">
+          <Routes>
+            <Route path="/" element={<YaziListesi />} />
+            <Route path="/posts/:id" element={<YaziDetayi />} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/YaziDetayi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const YaziDetayi = (props) => {
+  let { id } = useParams();
+  let [yaziDetayi, setYaziDetayi] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
+      .then((response) => {
+        setYaziDetayi(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <React.Fragment>
+      <h2 className="ui header">{yaziDetayi.title}</h2>
+      <p>{yaziDetayi.created_at}</p>
+      <p>{yaziDetayi.content}</p>
+    </React.Fragment>
+  );
+};
+
+export default YaziDetayi;
